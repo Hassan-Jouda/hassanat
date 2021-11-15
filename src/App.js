e55906ad47e593a40c95f2c2edc841a1ed2ab91b
Hassan Jouda <hassan.jd.2006@gmail.com>
2021-11-15 21:19:42 +0200

    https://github.com/Hassan-Jouda/hassanat.git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import react from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [hacker, setHacker] = useState(false);
+
+  function loss() {
+    setCount(count - 1);
+  }
+
+  function pls() {
+    setCount(count + 1);
+  }
+
+  function zero() {
+    setCount(count - count);
+  }
+
+  function Hacker() {
+    setCount(count + 10);
+  }
+
+  useEffect(() => {
+    if (hacker) {
+      const loop = setInterval(() => {
+        setCount(count + 1);
+      }, 10);
+      return () => {
+        clearInterval(loop);
+      };
+    }
+  });
+  function Hacker2() {
+    if (hacker) {
+      setHacker(false);
+      console.log("hacker: off");
+    } else {
+      setHacker(true);
+      console.log("hacker: on");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#09009B" }} className="App-header">
+      <h1>المسبحة الذكية فقط للهكر </h1>
+      <h1 className="hassanat">{count} عدد الحسنات</h1>
+
+      <button className="btn" onClick={loss}>
+        نقص حسنات
+      </button>
+      <button className="btn" onClick={pls}>
+        زود حسنات
+      </button>
+      <button className="btn" onClick={zero}>
+        صفر الحسنات
+      </button>
+
+      <h2 className="hacker">only Hacker </h2>
+      <button className="btn" onClick={Hacker}>
+        كل ضغطة 10 حسنات
+      </button>
+      <button className="btn" onClick={Hacker2}>
+        قلتش حسنات لانهائية{" "}
+      </button>
     </div>
   );
 }
